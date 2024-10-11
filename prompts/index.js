893ae9b53dd2d6a82f9bf956379385f38f5112f9
Evan Quirk <71678276+evanquirk@users.noreply.github.com>
2024-10-11 11:25:43 -0700

    add morning tasks to prompt index

--- prompts/index.js
+++ prompts/index.js
@@ -3,6 +3,10 @@ export { sleep } from './individual/sleep.js';
 export { dream } from './individual/dream.js';
 export { noteAM } from './individual/noteAM.js';
 export { wake } from './individual/wake.js';
+export { breakfast } from './individual/breakfast.js';
+export { workout } from './individual/workout.js';
+export { weight } from './individual/weight.js';
+export { tasks } from './individual/tasks.js';
 
 export { morning } from './series/morning.js';
 export { environment } from './series/environment.js';
